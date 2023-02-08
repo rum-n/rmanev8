b49f8d03daf0dbcae21cebab7a8e7b41ce01cdc7
Rumen Manev <rumen.manev@mova.ie>
2023-02-08 14:18:06 +0200

    status update

--- src/components/sidenav/Sidenav.js
+++ src/components/sidenav/Sidenav.js
@@ -1,30 +1,51 @@
 import React from "react";
-import './Sidenav.css';
-import { RiFocus3Line } from 'react-icons/ri';
-import { VscLoading } from 'react-icons/vsc';
-import { BiGlassesAlt } from 'react-icons/bi';
-import { RiMapPinLine } from 'react-icons/ri';
+import "./Sidenav.css";
+import { RiFocus3Line } from "react-icons/ri";
+import { VscLoading } from "react-icons/vsc";
+import { BiGlassesAlt } from "react-icons/bi";
+import { RiMapPinLine } from "react-icons/ri";
 
 export default function Sidenav() {
-
   return (
-    <div className='sidenav'>
-        <div>
-            <h2><span className='icon'><RiFocus3Line/></span>Focusing on</h2>
-            <p className='status-details'>Improving my Angular skills and learning some Java/Spring.</p>
-        </div>
-        <div>
-            <h2><span className='icon'><VscLoading/></span>Preparing for</h2>
-            <p className='status-details'>Winter season.</p>
-        </div>
-        <div>
-            <h2><span className='icon'><BiGlassesAlt/></span>Curious about</h2>
-            <p className='status-details'>Generative art with Processing and p5.js</p>
-        </div>
-        <div>
-            <h2><span className='icon'><RiMapPinLine/></span>Currently in</h2>
-            <p className='status-details'>Sofia, Bulgaria (GMT+2)</p>
-        </div>
+    <div className="sidenav">
+      <div>
+        <h2>
+          <span className="icon">
+            <RiFocus3Line />
+          </span>
+          Focusing on
+        </h2>
+        <p className="status-details">React & React Native and some Node.js</p>
+      </div>
+      <div>
+        <h2>
+          <span className="icon">
+            <VscLoading />
+          </span>
+          Preparing for
+        </h2>
+        <p className="status-details">Winter season.</p>
+      </div>
+      <div>
+        <h2>
+          <span className="icon">
+            <BiGlassesAlt />
+          </span>
+          Curious about
+        </h2>
+        <p className="status-details">
+          Generative art with Processing and p5.js
+        </p>
+      </div>
+      <div>
+        <h2>
+          <span className="icon">
+            <RiMapPinLine />
+          </span>
+          Currently in
+        </h2>
+        <p className="status-details">Sofia, Bulgaria (GMT+2)</p>
+      </div>
     </div>
   );
 }
